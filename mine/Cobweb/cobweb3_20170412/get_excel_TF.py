13db38b17717387e4eba21d8c6050704232aa127
Tongzhe Zhang <richard10ztz@gmail.com>
2017-08-10 11:02:04 +0800

    添加蜘蛛网策略

---
+++ mine/Cobweb/cobweb3_20170412/get_excel_TF.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 02 15:51:43 2017
+get excel of trading information, begin from 2011.4.16
+@author: Richard10ZTZ
+"""
+
+
+from time import sleep
+import urllib2
+
+def downloadexcel(url, year, month, day):
+    u=urllib2.urlopen(url)
+    data=u.read()
+    #print data
+    if "DOCTYPE" in data:
+        print "haven't this day", url
+    else:
+        #print "have this day"
+        newname = "TFdata/"+year+month+day+".csv"
+        f=open(newname,'wb')
+        f.write(data)
+        f.close()   
+  
+if __name__ == "__main__":
+    
+    for years in range(2013, 2018):
+        tempyear = str(years)
+        for monthes in range(1, 13):
+            if monthes < 10:
+                tempmonth = "0"+ str(monthes)
+            else:
+                tempmonth = str(monthes)
+            for days in range(1, 32):
+                if days < 10:
+                    tempday = "0"+ str(days)
+                else:
+                    tempday = str(days)
+                useurl = "http://www.cffex.com.cn/sj/ccpm/"+tempyear+tempmonth+"/"+tempday+"/TF_1.csv"
+                downloadexcel(useurl, tempyear, tempmonth, tempday)
