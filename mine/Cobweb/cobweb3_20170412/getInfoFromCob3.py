7bee5a35fc4d5892b1e23c63f1eef6abcbdd52e9
Tongzhe Zhang <richard10ztz@gmail.com>
2017-08-11 15:26:19 +0800

    蜘蛛网3

--- mine/Cobweb/cobweb3_20170412/getInfoFromCob3.py
+++ mine/Cobweb/cobweb3_20170412/getInfoFromCob3.py
@@ -12,34 +12,84 @@ def getInfoFromCob3(test_csv, tempdate):
     row = []
     for temprow in test_csv:
         row.append(temprow)
-    """find out the blank row"""
+        
+    """find out the blank row and we can get the first line"""
     flagblank = 0
-    beginrow = 0    
-    while flagblank == 0:
+    beginrow = []   
+    endrow = len(row)
+    while flagblank != 1:
         for i in range(0, 10):
             if row[i] == []:
-                print i,"is blank row"
-                beginrow = i+3
+                #print "blank row is:",i
+                if flagblank == 0: beginrow.append(i+3)
                 flagblank = 1
-    print beginrow," ",flagblank
+    #print "the first row is:", beginrow[0]," ",flagblank
+    """record the # of sheets"""
+    sheetsnum = 1
+    for i in range(beginrow[0]+1, endrow):
+        if row[i][2] == "1":
+            beginrow.append(i)
+            sheetsnum = sheetsnum + 1
+    #print "# of sheets:",sheetsnum
+    beginrow.append(endrow)
+
+    """which one is better"""
+    bestSheet = 0
+    tempV = 0
+    SheetsV = []
+    for i in range(sheetsnum-1): 
+        tempT = 0
+        for j in range(beginrow[i], beginrow[i+1]):
+           tempT = tempT + int(row[j][4])
+        SheetsV.append(tempT)
+    tempT = 0
+    for i in range(beginrow[sheetsnum-1], endrow):
+        tempT = tempT + int(row[i][4])
+    #print tempT
+    SheetsV.append(tempT)
+    for i in range(len(SheetsV)):
+        if SheetsV[i] > tempV:
+            tempV = SheetsV[i]
+            bestSheet = i
+    
+    #print "di", i+1, "sheet is better"
+    
     """record data of B, S"""
     Bincre = 0
     Sincre = 0
-    '''
-    for t in range(0,20):
-        Bincre = Bincre + int(row[beginrow+t][8])
-        Sincre = Sincre + int(row[beginrow+t][11])
-    return [Bincre, Sincre]
-    '''
+    Bnum = 0
+    Snum = 0
+    Vnum = 0
+    signal = 0
+    
+    for t in range(beginrow[bestSheet],beginrow[bestSheet+1]):
+        if int(row[t][4]) != 0:
+            Vnum = Vnum+1
+        if int(row[t][7]) != 0:
+            Bnum = Bnum+1
+        if int(row[t][10]) != 0:
+            Snum = Snum+1
+        Bincre = Bincre + int(row[t][8])
+        Sincre = Sincre + int(row[t][11])
+    
+    if Bincre - Sincre > 0:
+        signal = 1
+    elif Bincre - Sincre < 0:
+        signal = 2
+    if Bnum<20 or Snum< 20 or Vnum<20:
+        signal = 3
+  
+    #if endrow-beginrow < 20: signal = 4
+    #print "signal:", signal
+    return [Bincre, Sincre, signal, Vnum, Bnum, Snum, SheetsV,  beginrow]
+    
 """main function"""
 if __name__ == '__main__':
 
-    """read the csv file
+    """read the csv file"""
     
     recorddate = []
     recorddata = []
-    lastB = 0.0
-    lastS = 0.0
     for year in range(2010, 2018):
         tempyear = str(year)
         for month in range(1, 13):
@@ -49,33 +99,26 @@ if __name__ == '__main__':
                 if day < 10: tempday = "0" + str(day)
                 else: tempday = str(day)
                 tempdate = tempyear+tempmonth+tempday
-                tempname = "../IFdata/"+tempdate+".csv"
+                tempname = "../TFdata/"+tempdate+".csv"
                 try:
                     test_csv = csv.reader(open(tempname))
-                    result = getInfoFromCob(test_csv, tempdate)
-                    lastB = result[0]
-                    lastS = result[1]
+                    result = getInfoFromCob3(test_csv, tempdate)
+                    signal = result[2]
                     recorddate.append(tempdate)
-                    recorddata.append(result)
+                    recorddata.append(signal)
                     #print result
                 except IOError:
                     print tempdate," not have this day"
                     pass
-    """
-    """   testing """             
-    test_csv = csv.reader(open("../TFdata/20131212.csv"))
-    result = getInfoFromCob3(test_csv, "20131212")               
     
+    """   testing             
+    test_csv = csv.reader(open("../TFdata/20170609.csv"))#20170712
+    result = getInfoFromCob3(test_csv, "20170609")       #20131212    20170807
+    """ 
     
-    """write data into .txt
+    """write data into .txt"""
     w = open("finaldata.txt","w")
     for i in range(0, len(recorddate)):
-        key = 0
-        if recorddata[i][0]>0 and recorddata[i][1]<0:
-            key = 1
-        elif recorddata[i][0]<0 and recorddata[i][1]>0:
-            key = 2
-        else: key = 3
-        w.write(recorddate[i][2:]+" "+str(key)+"\n")
+        w.write(recorddate[i][2:]+" "+str(recorddata[i])+"\n")
     w.close()
-    """
+    
